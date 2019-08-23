ed7982371efe07b528e8bbb28d365189a4230bd0
Takur0 <yt1997kt@gmail.com>
2019-08-24 08:31:53 +0900

    Fix: Bugs [camelCase, pascalCase] functions

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Flutter BLoC File Generator
 
-"flutter-bloc". After writing up a brief description, we recommend including the following sections.
+"flutter-bloc". BLoC Pattern for Flutter Extention, writing Files.
 
-## Features
+## Rerease Notes
+- Work In Progress...
+<!-- ## Features
+
 
 Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
 
@@ -55,4 +58,4 @@ Added features X, Y, and Z.
 
 * Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
 * Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets -->
--- src/extension.ts
+++ src/extension.ts
@@ -56,62 +56,19 @@ async function inputNewFileName(){
 	return (await vscode.window.showInputBox({ prompt: 'Input New Screen files ID.', placeHolder: 'new screen files ID, e.g. splash-screen', value: '_screen' })) || '';
 }
 
-	
 function camelCase(str:string){
-	str = replaceAccents(str);
-	str = removeNonWord(str)
-		.replace(/\-/g, ' ') //convert all hyphens to spaces
-		.replace(/\s[a-z]/g, upperCase) //convert first char of each word to UPPERCASE
-		.replace(/\s+/g, '') //remove spaces
-		.replace(/^[A-Z]/g, lowerCase); //convert first char to lowercase
-	return str;
+	str = str.charAt(0).toLowerCase() + str.slice(1);
+	return str.replace(/[-_](.)/g, function(match, group1) {
+		return group1.toUpperCase();
+	});
 }
 
+
 function pascalCase(str:string){
-	return camelCase(str).replace(/^[a-z]/, upperCase);
+	var camel = camelCase(str);
+	return camel.charAt(0).toUpperCase() + camel.slice(1);
 }
   
-function lowerCase(str:string){
-	return str.toLowerCase();
-}
-  
-function upperCase(str:string){
-	return str.toUpperCase();
-}
-  
-  
-function replaceAccents(str:string){
-	// verifies if the String has accents and replace them
-	if (str.search(/[\xC0-\xFF]/g) > -1) {
-		str = str
-				.replace(/[\xC0-\xC5]/g, "A")
-				.replace(/[\xC6]/g, "AE")
-				.replace(/[\xC7]/g, "C")
-				.replace(/[\xC8-\xCB]/g, "E")
-				.replace(/[\xCC-\xCF]/g, "I")
-				.replace(/[\xD0]/g, "D")
-				.replace(/[\xD1]/g, "N")
-				.replace(/[\xD2-\xD6\xD8]/g, "O")
-				.replace(/[\xD9-\xDC]/g, "U")
-				.replace(/[\xDD]/g, "Y")
-				.replace(/[\xDE]/g, "P")
-				.replace(/[\xE0-\xE5]/g, "a")
-				.replace(/[\xE6]/g, "ae")
-				.replace(/[\xE7]/g, "c")
-				.replace(/[\xE8-\xEB]/g, "e")
-				.replace(/[\xEC-\xEF]/g, "i")
-				.replace(/[\xF1]/g, "n")
-				.replace(/[\xF2-\xF6\xF8]/g, "o")
-				.replace(/[\xF9-\xFC]/g, "u")
-				.replace(/[\xFE]/g, "p")
-				.replace(/[\xFD\xFF]/g, "y");
-	}
-  
-	return str;
-}
-function removeNonWord(str:string){
-	return str.replace(/[^0-9a-zA-Z\xC0-\xFF \-]/g, '');
-}
 
 function _generateBlocContent(rootPath:string, fileName:string){
   return `import 'package:bloc/bloc.dart';
